cdda86a68b7de8fb32b7912ef90a7e62dc707523
Abe Ellison <abe.ellison@gmail.com>
2022-02-09 08:40:28 -0700

    Add documentation to the configuration parameters

--- spherinder/config.py
+++ spherinder/config.py
@@ -1,5 +1,12 @@
+# Default internal data type for constructing operators on the spherinder basis
 internal_dtype = 'float128'
+
+# Default maximum number of processes for parallel computations
 max_processes = 16
+
+# Default setting for triangular truncation of the spherinder basis functions
 default_truncate = True
+
+# Default setting for normalization of the spherinder basis functions
 default_normalize = True
 
