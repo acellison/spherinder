58837c163393ea6008b5e2c50c788319af70876e
Abe Ellison <abe.ellison@gmail.com>
2023-05-31 11:21:35 -0600

    Update spherinder graphic generation

--- paper/spherinder_graphic.py
+++ paper/spherinder_graphic.py
@@ -5,6 +5,9 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import matplotlib.pyplot as plt
 
+import matplotlib
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+
 
 def plot_surface(ax, x, y, z, facecolors, alpha=1.0, edgecolor='none', **kwargs):
     edgecolor = kwargs.pop('edgecolor', 'none')
@@ -21,7 +24,7 @@ def plot_cylinder(ax, nphi, nz, cmap, zscale=1., radius=1., x0=0., y0=0., alpha=
 
 def main():
     m, l = 2, 3
-    nphi, ntheta = 40, 41
+    nphi, ntheta = 512, 256
 
     # Create the mesh
     equispaced_theta = True
@@ -51,32 +54,22 @@ def main():
     fcolors = (1-margin)*fcolors + margin/2
 
     def prepare_axes(ax, zlabel):
-        fontsize = 14
+        fontsize = 18
         ticks = np.linspace(-1,1,5)
-        ax.set_xlabel('x', fontsize=fontsize)
-        ax.set_ylabel('y', fontsize=fontsize)
+        ax.set_xlabel('$x$', fontsize=fontsize)
+        ax.set_ylabel('$y$', fontsize=fontsize)
+        ax.zaxis.set_rotate_label(False)
         ax.set_zlabel(zlabel, fontsize=fontsize)
         ax.set_xticks(ticks)
         ax.set_yticks(ticks)
         ax.set_zticks(ticks)
-
-        # make the panes transparent
-        ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-        ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-        ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-        # make the grid lines transparent
-        ax.xaxis._axinfo["grid"]['color'] =  (1,1,1,0)
-        ax.yaxis._axinfo["grid"]['color'] =  (1,1,1,0)
-        ax.zaxis._axinfo["grid"]['color'] =  (1,1,1,0)
-
-        ax.xaxis.set_axis_off()
-        ax.yaxis.set_axis_off()
+        ax.set_box_aspect([1,1,1])
 
     def save(fig, filename):
         filename = os.path.abspath(os.path.join('./figures/spherinder_graphic', filename))
         if not os.path.exists(os.path.dirname(filename)):
             os.makedirs(os.path.dirname(filename))
-        fig.savefig(filename, dpi=200)
+        fig.savefig(filename, dpi=300)
 
     # Create the figure
     if same_figure:
@@ -89,8 +82,7 @@ def main():
     # Plot the spherical harmonic on a sphere
     ax1 = fig.add_subplot(pos1, projection='3d')
     plot_surface(ax1, x, y, z, facecolors=cmap(fcolors))
-    prepare_axes(ax1, 'z')
-    fig.set_tight_layout(True)
+    prepare_axes(ax1, '$z$')
     if not same_figure:
         save(fig, 'spherinder_mapping-cartesian.png')
 
@@ -103,13 +95,12 @@ def main():
     ax2 = fig.add_subplot(pos2, projection='3d')
     plot_surface(ax2, x[:,:ntheta//2], y[:,:ntheta//2], 0*z[:,:ntheta//2]+1.0,  facecolors=cmap(  fcolors[:,:ntheta//2]))
     plot_surface(ax2, x[:,:ntheta//2], y[:,:ntheta//2], 0*z[:,:ntheta//2]-1.0,  facecolors=cmap(1-fcolors[:,:ntheta//2]))  # Hack for (s,z)==(0,-1)
-#    plot_surface(ax2, x[:,ntheta//2:], y[:,ntheta//2:], 0*z[:,ntheta//2:]-1.0,  facecolors=cmap(fcolors[:,ntheta//2:]))
     plot_cylinder(ax2, nphi, nz=ntheta, cmap=cmap, zscale=0.99, alpha=0.4)
-    prepare_axes(ax2, 'η')
     ax2.view_init(elev=elev, azim=azim)
     ax2.set_xlim3d(xlim)
     ax2.set_ylim3d(ylim)
     ax2.set_zlim3d(zlim)
+    prepare_axes(ax2, r'$\eta$')
     fig.set_tight_layout(True)
 
     if not same_figure:
